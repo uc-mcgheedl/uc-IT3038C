eb828f2f9056dc65192cc6cc4ba12e297c802b7e
mcghee <mcgheedl@mail.uc.edu>
2020-02-04 18:42:50 -0500

    adding powershell files

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,9 @@
+﻿function getIP {
+    (Get-NetIPAddress).ipv4address | Select-String "192*"
+}
+
+
+$IP = getIP
+
+write-host("This machines IP is $IP")
+write-host("This machines IP is {0}" -f $IP)
---
+++ powershell/windowssysinfo.ps1
@@ -0,0 +1,14 @@
+﻿function getIP {
+    (Get-NetIPAddress).ipv4address | Select-String "192*"
+}
+
+$IP = getIP
+$myname = hostname
+$tdy = date
+
+$BODY = "This machines IP is $IP.User is Administrator .Hostname is $myname. Powershell Version is 5.Today's date is $tdy."
+write-host ("$BODY")
+Send-MailMessage -To "mcgheedl@mail.uc.edu" -From "me@mail.uc.edu" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -Port 587 -UseSsl -Credential (Get-Credential)
+
+Write-Host ("Email Sent!")
+
