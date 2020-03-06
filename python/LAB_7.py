048466aab7035612d7f9aa1dbb33d80bb7da465a
uc-mcgheedl <59898837+uc-mcgheedl@users.noreply.github.com>
2020-03-05 21:59:28 -0500

    Lab_7 code

---
+++ python/LAB_7.py
@@ -0,0 +1,37 @@
+
+from flashtext import KeywordProcessor
+
+#Remove the comment marks for each block of code individually
+
+#checks the number of times a keyword has appeared
+
+#keyword_processor = KeywordProcessor()
+#keyword_dict = {
+#    "compliance management": ["HR", "human resources"],
+#     "product management": ["PM", "product manager"]
+# }
+#keyword_processor.add_keywords_from_dict(keyword_dict)
+#print(len(keyword_processor))
+
+
+#checks if a keyword is present
+
+#keyword_processor = KeywordProcessor()
+#keyword_processor.add_keyword('All Father', 'God')
+#praise = 'All Father' in keyword_processor
+#print(praise)
+#jesus = keyword_processor.get_keyword('All Father')
+#print(jesus)
+
+
+
+# extracts keywords in a txt file
+
+# with open('vacation.txt') as sentence:
+#     content = sentence.read()
+# keyword_processor = KeywordProcessor()
+# keyword_processor.add_keyword('Big Apple')
+# keyword_processor.add_keyword('Bay Area')
+# print(keyword_processor.extract_keywords(content))
+
+
