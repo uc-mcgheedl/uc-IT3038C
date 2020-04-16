4d51b177a89e9f54ce7386553d7dbe4884cd4ad1
uc-mcgheedl <59898837+uc-mcgheedl@users.noreply.github.com>
2020-04-16 17:02:36 -0400

    updated

--- node/api.js
+++ node/api.js
@@ -3,11 +3,12 @@ var data = require("D:/widgets.json");
 
 var server = http.createServer(function(req, res){
     if (req.url === "/") {
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.end("Foundit");
+    }
+    else if (req.url === "/api") {
         res.writeHead(200, {"Content-Type": "text/json"});
         res.end(JSON.stringify(data));
-    }
-    else if (req.url === "/blue") {
-        listBlue(res);
     } else {
         res.writeHead(404, {"Content-Type": "text/plain"});      
         res.end("Data not found");        
@@ -18,10 +19,5 @@ var server = http.createServer(function(req, res){
 server.listen(3000);
 console.log("Server is listening on port 3000");
 
-function listBlue(res) {
-    var colorBlue = data.filter(function(item) {
-        return item.color === "blue";
-    });
 
-    res.end(JSON.stringify(colorBlue));
-}
+
